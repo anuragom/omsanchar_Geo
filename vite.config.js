d34f7392ffae338137f0aa46b6583d9e3dcbe23d
omlogistic <ankit.pandey@omlogistics.co.in>
2025-04-16 12:36:10 +0530

    add vite config

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5000,  // Set default port to 5000
+    allowedHosts: [
+      'trackinguat.omlogistics.co.in',  // Add your specific host here
+      // You can add other allowed hosts if needed
+    ],
   },
 });
